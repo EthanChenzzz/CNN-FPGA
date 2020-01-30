766465ef125bbb7925c62c0ef7c67e35c5262408
EthanChenzzz <47748098+EthanChenzzz@users.noreply.github.com>
2020-01-30 15:17:32 +0800

    Update Max.v

--- src/Max.v
+++ src/Max.v
@@ -25,18 +25,23 @@ module Max#(
     parameter LENGTH = 4
     )
     (
+    //input: BITWIDTH(一個數字含有BITWIDTH個bits) * Pool_Size。
+    //output: 在該Pool中最大的數字。
     input [BITWIDTH * LENGTH - 1 : 0] data,
     output reg signed [BITWIDTH - 1 : 0] result
     );
     
     wire signed [BITWIDTH - 1:0] dataArray[0:LENGTH - 1];
     genvar i;
+    
+    // 將input的所有數字賦值到一個dataArray方便處理。
     generate      
         for(i = 0; i < LENGTH; i = i + 1) begin
             assign dataArray[i] = data[i * BITWIDTH + BITWIDTH - 1: i * BITWIDTH];
         end
     endgenerate
     
+    // 簡單的for loop 找最大值。
     integer j;
     always @(*) begin
         result = -127;
