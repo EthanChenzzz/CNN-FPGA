3eb46a294d8f3227ec5f9617d43a5ce90c05126f
EthanChenzzz <47748098+EthanChenzzz@users.noreply.github.com>
2020-01-30 15:39:07 +0800

    Update Max_pool.v

--- src/Max_pool.v
+++ src/Max_pool.v
@@ -27,21 +27,27 @@ module Max_pool#(
     parameter integer DATAHEIGHT = 28,
     parameter integer DATACHANNEL = 3,
     
+    //Pool_Size
     parameter integer KWIDTH = 2,
     parameter integer KHEIGHT = 2
     )
     (
     //input clk,
     //input clken,
+    //input: 某個Map，包含長寬深(分別是這邊的DATAWIDTH、DATAHEIGHT、DATACHANNEL)。
+    //output: 某個Map經過Pooling的結果，size = Map_size / Pool_Size
     input [BITWIDTH * DATAWIDTH * DATAHEIGHT * DATACHANNEL - 1 : 0]data,
     output reg [BITWIDTH * DATAWIDTH / KWIDTH * DATAHEIGHT / KHEIGHT * DATACHANNEL - 1 : 0] result
     );
     
+    //每一個dataArray的element的size是BITWIDTH。
+    //每一個paramArray的element的size是BITWIDTH * KHEIGHT * KWIDTH。
     wire [BITWIDTH - 1 : 0] dataArray[0 : DATACHANNEL - 1][0 : DATAHEIGHT-1][0 : DATAWIDTH - 1];
     wire [BITWIDTH * KHEIGHT * KWIDTH - 1 : 0]paramArray [0: DATACHANNEL - 1][0: DATAHEIGHT / KHEIGHT - 1][0: DATAWIDTH / KWIDTH - 1];
     
     //wire [BITWIDTH * DATAWIDTH / KWIDTH * DATAHEIGHT / KHEIGHT * DATACHANNEL - 1 : 0] out;
     
+    //單純賦值，將input的所有數字賦值到一個dataArray方便處理。
     genvar i, j, k, m, n;
     generate       
         for(i = 0; i < DATACHANNEL; i = i + 1) begin
@@ -52,6 +58,7 @@ module Max_pool#(
             end
         end      
     endgenerate
+    
     
     generate 
         for(i = 0; i < DATACHANNEL; i = i + 1) begin
@@ -67,6 +74,7 @@ module Max_pool#(
         end
     endgenerate
     
+    //一個一個將paramArray的所有element[每一個element(element map)的size是BITWIDTH * KHEIGHT * KWIDTH]丟到MAX module，求出每一個element map的最大值。
     generate
         for(i = 0; i < DATACHANNEL; i = i + 1) begin
             for(j = 0; j < DATAHEIGHT / KHEIGHT; j = j + 1) begin
